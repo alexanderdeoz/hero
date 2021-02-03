5b20f7327ee16e6c8951e0f0944f4bdc22a84968
alexanderdeoz <sad.chinchin@yavirac.edu.ec>
2021-02-03 12:28:55 -0500

    heros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+//archivo que inicia la aplicacion
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { VillanosComponent } from './villanos/villanos.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';//para que importe la plantilla
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    VillanosComponent,
+    HeroDetailComponent,
+    MessagesComponent//componente importado
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
